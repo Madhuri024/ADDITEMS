56a45503efccf7d2d4ba30b4f2ad7e90a0139f0c
Madhuri024 <rekhamadhuri95@gmail.com>
2022-07-18 23:40:23 +0530

    commit

---
+++ dom.js
@@ -0,0 +1,24 @@
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
+var headertitle =document.getElementById('header-title');
+
+//console.log(headertitle);
+//headertitle.textContent='Hello';
+//headertitle.innerText = 'goodbye';
+//console.log(headertitle.textContent);
+//console.log(headertitle.innerText);
+//headertitle.innerHTML='<h3>Hello</h3>';
+headertitle.style.borderBottom = 'solid 3px #000';
+var t =document.querySelector('.title');
+t.style.color ='green';
+t.style.fontWeight ='bold';
+
